0ee9c838cc221d7fdebeb66323778fff8b669715
Adeola Tawakalitu Adenike <tawa@Adeolas-MacBook-Pro.local>
2023-02-03 16:47:53 +0100

    adjustment

--- src/components/pickAddons.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="py-4" v-for="checkbox in chechboxes" :key="checkbox"> 
-        <InputForm class="pt-10 border border-purplish-blue rounded-md h-14"/>
-    </div>
-</template>
-<script setup lang="ts">
-import InputForm from './InputForm.vue';
-</script>
